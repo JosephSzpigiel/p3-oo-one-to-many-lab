a1fc30797fe715da2790314c4e33c1d295b80d81
Joseph Szpigiel <josephszpigiel@gmail.com>
2023-10-26 11:32:02 -0400

    lab complete

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,40 @@
+from operator import attrgetter
+
 class Pet:
-    pass
+    all = []
+
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+
+    def __init__(self, name, pet_type, owner = None):
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        Pet.all.append(self)
+    
+    @property
+    def pet_type(self):
+        return self._pet_type
+
+    @pet_type.setter
+    def pet_type(self, type):
+        if type in Pet.PET_TYPES:
+            self._pet_type = type
+        else:
+            raise ValueError('Not a valid pet type!')
+
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        return [p for p in Pet.all if p.owner == self]
+    
+    def add_pet(self, pet):
+        if isinstance(pet, Pet):
+            pet.owner = self
+        else:
+            raise ValueError('Must be an instance of Pet!')
+        
+    def  get_sorted_pets(self):
+        return sorted(self.pets(), key=attrgetter('name'))
